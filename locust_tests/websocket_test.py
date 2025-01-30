84bad6b12dc3606d8cb12f3c3e6f545108deaa28
Gavin Hewitt <ghewitt@email.sc.edu>
2025-01-30 11:08:46 -0500

    testing attempts

--- locust_tests/websocket_test.py
+++ locust_tests/websocket_test.py
@@ -16,9 +16,17 @@ class WebSocketStandardsListener(User):
         asyncio.run(self.listen())
 
 class WebSocketPollSender(User):
-    def on_start(self):
+    def __init__(self, environment):
+        super().__init__(environment)
+        self.loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(self.loop)
         self.user_id = self.environment.runner.user_count
-    
+
+    @task
+    def execute_send(self):
+        self.loop.run_until_complete(self.send_message())
+        self.environment.runner.quit()
+
     async def send_message(self):
         print(f'{self.user_id} sending message')
         async with websockets.connect(WS_URL) as ws:
@@ -26,7 +34,24 @@ class WebSocketPollSender(User):
             await ws.send(message)
             print(f'{self.user_id} message sent')
 
+from locust import User, task, events
+from geventwebsocket import websocket
+import gevent
+
+class WebSocketUser(User):
+    def on_start(self):
+        """Establish WebSocket connection at the start of the test."""
+        self.ws = websocket.WebSocket()
+        self.ws.connect(WS_URL)
+
     @task
-    def execute_send_message(self):
-        asyncio.run(self.send_message())
-        self.environment.runner.quit()
+    def send_message(self):
+        """Send and receive messages over WebSocket."""
+        self.ws.send("Hello WebSocket!")
+        response = self.ws.recv()
+        print(f"Received: {response}")
+        gevent.sleep(1)  # Simulate think time
+
+    def on_stop(self):
+        """Close WebSocket connection at the end."""
+        self.ws.close()
--- voting_be/poll/tests.py
+++ voting_be/poll/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from channels.testing import WebsocketCommunicator
+from .consumers import PollConsumer
 
 # Create your tests here.
+class StandardsTest(TestCase):
+    async def test_receive(self):
+        communicator = WebsocketCommunicator(PollConsumer.as_asgi(), "/ws/poll/testing/")
+        connected, subprotocol = communicator.connect()
+        assert connected
+
+        while True:
+            message = await communicator.receive_json_from()
+            print(message)
